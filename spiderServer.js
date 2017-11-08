721d2ee429c4d580b4487948ceb00821dadf54d8
xijing <xijing@rongzi.com>
2017-11-08 16:32:47 +0800

    1

---
+++ spiderServer.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3030
+
+app.set('port', port)
+
+app.use('*',function (req, res, next) {
+    if (req.xhr || req.baseUrl.match(/(.js|.css|.ico|.png|.jpg|.jpeg|.gif|.xml|.svg)$/g) || req.baseUrl.match(".css")) {
+        res.status(404).send('Sorry cant find that') 
+    } else {
+        next()
+    }
+});
+
+// 设置api routes
+app.use('/', require('./src/staticPage'))
+
+// 设置本地上传文件的静态资源服务
+if (process.env.NODE_ENV === "development"){
+    app.use(express.static(__dirname + '/dist'))
+    console.log('server static resource at '+ __dirname + '/dist')
+}
+
+app.listen(port, host)
+console.log('app listening at http://%s:%s',host,port)
---
+++ src/staticPage.js
@@ -0,0 +1,65 @@
+const router = require('express').Router()
+const phantom = require('phantom')
+const fs = require('fs')
+const path = require('path')
+const pageDir = 'cache'
+
+phantom.outputEncoding = 'utf-8'
+phantom.scriptEncoding = 'utf-8'
+/* GET home page. */
+router.get('*', (req, res, next) => {
+    let sitepage = null
+    let phInstance = null
+    let fileName = (req.hostname + req.path).replace(/\/|\:/g,'_')
+    fs.exists(path.join(__dirname,pageDir,fileName), function(exists) {
+        if(exists){
+            fs.readFile(path.join(__dirname,pageDir,fileName), function(err,data) {
+                if (err) {
+                    return console.error(err)
+                }
+                res.send(data.toString())
+            })
+        } else {
+            phantom.create()
+                .then(instance => {
+                    phInstance = instance
+                    return instance.createPage()
+                })
+                .then(page => {
+                    sitepage = page
+                    //return page.open('http://localhost:8080' + req.path)
+                    return page.property('onLoadStarted')
+                })
+                .then(status => {
+                    return sitepage.injectJs('./node_modules/babel-polyfill/dist/polyfill.js')
+                })
+                .then(status => {
+                    // return sitepage.open(req.protocol + '://' + 'www.baidu.com')
+                    return sitepage.open(req.protocol + '://' + req.hostname + req.path)
+                })
+                .then(status => {
+                    console.log('status is: ' + status)
+                    if (status === 'success') {
+                        return sitepage.property('content')
+                    }
+                })
+                .then(content => {
+                    //console.log(content)
+                    res.send(content)
+                    fs.writeFile(path.join(__dirname,pageDir,fileName), content,  function(err) {
+                        if (err) {
+                            return console.error(err)
+                        }
+                    })
+                    sitepage.close()
+                    phInstance.exit()
+                })
+                .catch(error => {
+                    console.log(error)
+                    phInstance.exit()
+                })
+        }
+    })
+})
+
+module.exports = router
