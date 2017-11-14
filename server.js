e4e7812889505f0621eee30e476921e17dac0b5a
xijing <xijing@rongzi.com>
2017-11-14 11:50:34 +0800

    1

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3030
 
 app.set('port', port)
 
+// 过滤静态资源请求
 app.use('*',function (req, res, next) {
     if (req.xhr || req.baseUrl.match(/(.js|.css|.ico|.png|.jpg|.jpeg|.gif|.xml|.svg)$/g)) {
         res.status(404).send('Sorry cant find that') 
@@ -14,8 +15,8 @@ app.use('*',function (req, res, next) {
     }
 });
 
-// 设置api routes
-app.use('/', require('./src/staticPage2'))
+// 处理页面请求
+app.use('/', require('./src/dealpage'))
 
 // 设置本地上传文件的静态资源服务
 if (process.env.NODE_ENV === "development"){
